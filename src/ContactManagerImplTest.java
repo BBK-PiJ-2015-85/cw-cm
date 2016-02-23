143cd82718e8eb03f5e48b3d2e4d7d1e7364977f
BBK-PiJ-2015-85 <picklesj85@gmail.com>
2016-02-23 14:17:43 +0000

    added more tests for addNewContact

--- src/ContactManagerImplTest.java
+++ src/ContactManagerImplTest.java
@@ -1,6 +1,10 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by James Pickles on 23/02/2016.
  */
@@ -35,4 +39,24 @@ public class ContactManagerImplTest {
     public void testsAddEmptyNotes() {
         cm.addNewContact("Bob", "");
     }
+
+    @Test
+    public void testsAddOneNewContact() {
+        assertEquals(1, cm.addNewContact("Bob", "First Contact");
+    }
+
+    @Test
+    public void testsAddTwoNewContacts() {
+        cm.addNewContact("Tim", "First Contact");
+        assertEquals(2, cm.addNewContact("Bob", "Second Contact");
+    }
+
+    @Test
+    public void testsAddThreeNewContacts() {
+        cm.addNewContact("Tim", "First Contact");
+        cm.addNewContact("Jo", "Second Contact")
+        assertEquals(3, cm.addNewContact("Bob", "Third Contact");
+    }
+
+
 }
