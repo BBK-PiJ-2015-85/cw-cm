4fc2c6a34d7ef161ccfc980ca474422bbb769443
BBK-PiJ-2015-85 <picklesj85@gmail.com>
2016-03-02 16:11:40 +0000

    added methods for DateInstance class.

---
+++ src/DateInstanceImpl.java
@@ -0,0 +1,26 @@
+import java.util.Calendar;
+
+/**
+ * Created by James Pickles on 02/03/2016.
+ */
+public class DateInstanceImpl implements DateInstance {
+    private boolean dateChanged;
+    private Calendar fakeDate;
+
+    public Calendar getDateInstance() {
+        if (!dateChanged) {
+            return Calendar.getInstance();
+        } else {
+            return fakeDate;
+        }
+    }
+
+    public void changeDate(Calendar newDate) {
+        fakeDate = newDate;
+        dateChanged = true;
+    }
+
+    public void reset() {
+        dateChanged = false;
+    }
+}
