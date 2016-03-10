30d0f6403be21743ac91e6041a0203a32e09b264
BBK-PiJ-2015-85 <picklesj85@gmail.com>
2016-03-10 12:06:42 +0000

    separated import statements.

--- src/ContactManagerImpl.java
+++ src/ContactManagerImpl.java
@@ -1,5 +1,16 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
